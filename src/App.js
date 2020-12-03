72ff2608cdf7f77c22d56ffc83c0fcc07e75aed5
Kevin Peraza <kevin.peraza@live.com>
2020-12-02 23:09:13 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+//import cx from "classnames";
+import React, { useReducer } from "react";
+
+const useUILogic = () => {
+  const initialState = {
+    likes: 100,
+    liked: false
+  };
+
+  const reducer = (state, action) => {
+    switch (action) {
+      case "like":
+        return { ...state, likes: state.likes + 1, isBtnLiked: true };
+      case "dislike":
+        return { ...state, likes: state.likes - 1, isBtnLiked: false };
+      default:
+        throw new Error("Invalid reducer action");
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const like = () => dispatch("like");
+
+  const dislike = () => dispatch("dislike");
+
+  return {
+    commands: { like, dislike },
+    queries: { ...state }
+  };
+};
+
+const LikeButton = () => {
+  const {
+    commands: { like, dislike },
+    queries: { isBtnLiked, likes }
+  } = useUILogic();
+
+  return (
+    <>
+      <button
+        onClick={isBtnLiked ? dislike : like}
+        className={`like-button ${isBtnLiked ? "liked" : null}`}
+      >
+        <h2>
+          {isBtnLiked ? "Liked" : "Like"} | {likes}
+        </h2>
+      </button>
+      <style>{`
+                    .like-button {
+                        font-size: 1rem;
+                        padding: 5px 10px;
+                        color:  #585858;
+                        min-width: 150px;
+                    }
+                   .liked {
+                        font-weight: bold;
+                        color: #1565c0;
+                        border: 2px solid #1565c0;
+                   }
+            `}</style>
+    </>
+  );
+};
+
+export default LikeButton;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import LikeButton from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <LikeButton />
+  </React.StrictMode>,
+  rootElement
+);
